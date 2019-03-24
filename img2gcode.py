70808b1db94987d5f46ba59437595bc9e39d4773
vnazaren <volodymyr.nazarenko@globallogic.com>
2019-03-25 01:12:07 +0200

    Refactored Linear mode, added ability to tune speeds, fixed few bugs

--- img2gcode.py
+++ img2gcode.py
@@ -20,6 +20,11 @@ laser_mm = 0.1
 laser_on = "M106 S1"
 laser_off = "M107"
 
+# Speed to reach first point of track
+laser_fast = 1500
+# Speed of laser during engraving
+laser_slow = 900
+
 # GCode header / footer
 gcode_header = '''M107 ; disable FAN
 M106 L1 S0 ; switch FAN from PWM to On/Off mode (require Marlin code change)
@@ -43,12 +48,12 @@ M0         ; unconditional stop of printer
 # -------------- Arguments parsing, verification, internal common calculations ----------
 # user arguments definitions
 parser = argparse.ArgumentParser(description='Converting image to GCode for laser engraving')
-parser.add_argument('--image',       dest='image_filename', help='Image filename / file path',                        type=str,   required=True)
-parser.add_argument('--image_x_mm',  dest='image_x_mm',     help='Image width in mm',                                 type=float, required=True)
-parser.add_argument('--image_y_mm',  dest='image_y_mm',     help='Image height in mm',                                type=float, required=True)
-parser.add_argument('--gcode',       dest='gcode_filename', help='GCode filename / file path to store to',            type=str,   required=True)
-parser.add_argument('--laser_mm',    dest='laser_mm',       help='Laser dot size in mm (%fmm is default)' % laser_mm, type=float, required=False, default=laser_mm)
-parser.add_argument('--vector_mode', dest='vector_mode',    help='Switching between Vector (1) and Linear modes (0)', type=int,   required=False, default=0)
+parser.add_argument('--image',           dest='image_filename', help='Image filename / file path',                         type=str,   required=True)
+parser.add_argument('--image_x_mm',      dest='image_x_mm',     help='Image width in mm',                                  type=float, required=True)
+parser.add_argument('--image_y_mm',      dest='image_y_mm',     help='Image height in mm',                                 type=float, required=True)
+parser.add_argument('--gcode',           dest='gcode_filename', help='GCode filename / file path to store to',             type=str,   required=True)
+parser.add_argument('--laser_mm',        dest='laser_mm',       help='Laser dot size in mm (%fmm is default)' % laser_mm,  type=float, required=False, default=laser_mm)
+parser.add_argument('--linear_mode',     dest='linear_mode',    help='Switching between Vector (0) and Linear modes (1)',  type=int,   required=False, default=0)
 args = parser.parse_args()
 
 # checking if image is compatible etc
@@ -58,10 +63,11 @@ try:
     image_original = cv2.imread(args.image_filename)
     if image_original is None:
         raise Exception("Not a valid image file was provided")
-    # getting image dimentions and converting it to greyscale (Y, X, not interesting)
-    image_original_size = image_original.shape
+    # getting image dimentions and converting it to greyscale. [Y-rows, X-columns] - pay attention ;)
     image_original_grey = cv2.cvtColor(image_original, cv2.COLOR_BGR2GRAY)
-    # searching the borders of shapes and contours of them.
+    image_original_size = image_original_grey.shape
+    
+    # searching the all the borders of shapes and contours of them. This is needed for both Vector and Linear modes.
     # contours_main is array of all available shapes external contours, hierarchy_main is dependencies list between them
     thresh_original = cv2.threshold(image_original_grey, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     contours_main, hierarchy_main = cv2.findContours(thresh_original, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
@@ -112,14 +118,17 @@ print("Size of image, mm: %.2f %.2f" % (args.image_x_mm, args.image_y_mm))
 print("Thickness of laser, mm: %f" % args.laser_mm)
 print("Thickness of laser to be used for internal shape fill, px: %i" % dots_per_laser)
 print("Thickness of shapes body that will be removed from outer edge of each shape, px: %i" % dots_per_laser_outer_edge)
-print("Contours found: %i" % len(contours_main))
 #  -------------- End of reporting ----------
+
+
+#  -------------- Doing vecor pre-processing of the image to 
+
 
 # Opening gcode file in write mode, writing a header
 gcode_file = open(args.gcode_filename,"w")
 gcode_file.write(gcode_header)
 
-if (args.vector_mode == 0):
+if (args.linear_mode == 0):
     # -------------- Vector laser track searching ----------
     # algorithm of laser tracks generation is following:
     # 1. removing the outer edge of the shapes.
@@ -155,8 +164,11 @@ if (args.vector_mode == 0):
         if (hierarchy_main[0][main_contour_index][2] > -1):
             next_main_subcontour = hierarchy_main[0][main_contour_index][2]
             cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (255,255,255), -1)
+            cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (255,255,255), dots_per_laser_outer_edge)
+            
             while next_main_subcontour > -1:
-                cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (255,255,255), -2)
+                cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (255,255,255), -1)
+                cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (255,255,255), dots_per_laser_outer_edge)
                 next_main_subcontour = hierarchy_main[0][next_main_subcontour][0]
 
         # removing outer border of half laser size to determine first needed track over shape
@@ -179,15 +191,18 @@ if (args.vector_mode == 0):
 
         # incrementing main contour counter
         main_contour_index += 1
-    # -------------- End of vector laser track searching ----------
+    
+    print("Shapes found in total: %i" % len(shapes))
+    
+    # -------------- End of vector laser tracks searching ----------
 
     # -------------- GCode generation based on vector tracks ----------
     # algorithm of gcode generation is following:
-    # 1. adding header
-    # 2. going over the shapes one-by-one and converting pixels positions to mm positions
-    # 3. writing the mm positions to gcode file
+    # 1. going over the shapes one-by-one and converting pixels positions to mm positions
+    # 2. writing the mm positions to gcode file
     # 3. adding footer
 
+    print("Doing GCode generation")
     # going over identified shapes
     for shape in shapes:
         # last_dot indicating last place where laser was
@@ -209,7 +224,7 @@ if (args.vector_mode == 0):
                             gcode_file.write("%s \n" % laser_off)
 
                         # going to the start point
-                        gcode_file.write("G1 X%.2f Y%.2f F1500\n" % ((dot[0][0] * mm_per_dot), (dot[0][1] * mm_per_dot)))
+                        gcode_file.write("G1 X%.2f Y%.2f F%i\n" % ((dot[0][0] * mm_per_dot), (dot[0][1] * mm_per_dot), laser_fast))
 
                         # if far jump - enabling laser after start point was reached
                         if not near_laser_jump:
@@ -217,69 +232,120 @@ if (args.vector_mode == 0):
 
                     # if not the first poinit - just moving to it, nothing more to be done
                     else:
-                        gcode_file.write("G1 X%.2f Y%.2f F900\n" % ((dot[0][0] * mm_per_dot), (dot[0][1] * mm_per_dot)))
+                        gcode_file.write("G1 X%.2f Y%.2f F%i\n" % ((dot[0][0] * mm_per_dot), (dot[0][1] * mm_per_dot), laser_slow))
 
                     # storing current dot as the latest one
                     latest_dot = dot
 
                 # Returning to initial startpoint of the track in case if it is not straight line and if distance is more than 2*laserdot between any of axis
                 if (len(track) > 2) and ((abs(track[0][0][0] - dot[0][0]) > dots_per_laser * 2) or (abs(track[0][0][1] - dot[0][1]) > dots_per_laser * 2)):
-                    gcode_file.write("G1 X%.2f Y%.2f F900\n" % ((track[0][0][0] * mm_per_dot), (track[0][0][1] * mm_per_dot)))
+                    gcode_file.write("G1 X%.2f Y%.2f F%i\n" % ((track[0][0][0] * mm_per_dot), (track[0][0][1] * mm_per_dot), laser_slow))
 
                     # storing current dot as the latest one
                     latest_dot = track[0]
+    
+    print("GCode generation is done")
+    # -------------- End og GCode generation based on vector tracks ----------
 else:
     # -------------- Linear laser track searching ----------
     print("Working in Linear mode")
 
-    # TODO: this mode pending refactoring
-    # going over all lines (Y) one by one and searching if there's something we should print
-    data_lines = {}
-    line = 0
-    while line < image_original_size[0]:
-        line_data = [image_original_grey[line, column] for column in range(0, image_original_size[1], dots_per_laser)]
-        if line_data.count(255):
-            data_lines[line]=[]
-        line+=dots_per_laser
+    # Performing image pre-processing. We need to decrease all borders by half of laser size and be not dependent on choosen colours
+    print("Doing image pre-processing and decreasing shapes borders per half of laser beam diameter")
 
-    # going over all columns (X) one by one and searching where's something we should print
-    for line in data_lines.keys():
-        line_start = 0
-        column = 0
-        while column < image_original_size[1]:
-            point_color = image_original_grey[line, column]
-            if point_color == 0 and line_start == 0:
-                line_start = column
-            if point_color == 255 and line_start > 0:
-                data_lines[line].append([line_start, column])
-                line_start = 0
-            column+=dots_per_laser
+    # creating a blank white image
+    re_created_image = numpy.zeros([image_original_size[0], image_original_size[1]]).astype(numpy.uint8)
+    re_created_image.fill(255)
 
-    # inverting to let the laser go as a snake
+    main_contour_index = 0
+    for contour in contours_main:
+        # checking if this is internal closing contour of any of external contours. If yes - no need to handle it.
+        if (hierarchy_main[0][main_contour_index][3] > -1):
+            main_contour_index+=1
+            continue
+
+        # creating blank black image for our shape
+        main_contour_img_tmp = numpy.zeros([image_original_size[0], image_original_size[1]]).astype(numpy.uint8)
+        main_contour_img_tmp.fill(0)
+
+        # drawing the main contour fully filled inside with white in order to present our main shape
+        cv2.drawContours(main_contour_img_tmp, [contour], -1, (255,255,255), -1)
+
+        # if contour has closing contour (for example - it's rounding ground or has holes) - drawing it with all internals emptied properly
+        if (hierarchy_main[0][main_contour_index][2] > -1):
+            next_main_subcontour = hierarchy_main[0][main_contour_index][2]
+            # drawing contour filled with black inside and also a border of half of laser thickness since it will walk on outer edge
+            cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (0,0,0), -1)
+            cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (0,0,0), dots_per_laser_outer_edge)
+            while next_main_subcontour > -1:
+                # drawing contour filled inside with black and also a border of half of laser thickness since it will walk on outer edge
+                cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (0,0,0), -1)
+                cv2.drawContours(main_contour_img_tmp, contours_main, next_main_subcontour, (0,0,0), dots_per_laser_outer_edge)
+                next_main_subcontour = hierarchy_main[0][next_main_subcontour][0]
+
+        # removing outer border of half laser size to determine first needed track over shape
+        cv2.drawContours(main_contour_img_tmp, [contour], -1, (0,0,0), dots_per_laser_outer_edge)
+        
+        # getting a bitmask of our shape
+        threshold = cv2.threshold(main_contour_img_tmp, 0, 255, cv2.THRESH_BINARY)[1]
+        
+        # applying it to our resulting image with XOR operation
+        re_created_image = cv2.bitwise_xor(main_contour_img_tmp, re_created_image, threshold)
+    
+    # Replacing our original image with recreated
+    image_original_grey = re_created_image
+
+    print("Searching for laser start-stop points. Precision on Y axis is equal to laser beam diameter.")
+    # walking over all Y-lines and searching for start-stop points of laser beam into y_startstops dictionary per-line
+    # step for Y axis is chosen equal to laser beam size dots_per_laser
+    y_startstops = {}
+    for y in range(0, image_original_size[0], dots_per_laser):
+        beam_start = 0
+        beam_start_detected = 0
+        # walking over X axis and checking each pixel
+        for x in range(0, image_original_size[1]):
+            point_colour = image_original_grey[y, x]
+            # beam start point detection
+            if point_colour == 0 and not beam_start_detected:
+                beam_start = x
+                beam_start_detected = 1
+                continue
+            # beam end point detection
+            if point_colour == 255 and beam_start_detected:
+                if y not in y_startstops.keys():
+                    y_startstops[y] = []
+                y_startstops[y].append([beam_start, x])
+                beam_start_detected = 0
+
+    print("Doing GCode generation")
+    # inverting of path direction is used to let the laser go from the side where he was when previous Y-line finished
     inverted = False
-    for line in sorted(data_lines.keys()):
-        tracks = data_lines[line]
-        tracks_passed = 0
-        tracks_available = len(tracks)
-        while tracks_passed < tracks_available:
-            if inverted:
-                track = tracks[tracks_available - tracks_passed - 1]
-                gcode_file.write("G1 X%.2f00 Y%.2f00 F1500\n" % ((track[1] * mm_per_dot), (line * mm_per_dot)))
-                gcode_file.write("M106 S1 \n")
-                gcode_file.write("G1 X%.2f00 Y%.2f00 F900 \n" % ((track[0] * mm_per_dot), (line * mm_per_dot)))
-                gcode_file.write("M107 \n")
+    for y in sorted(y_startstops.keys()):
+        x_tracks = y_startstops[y]
+        x_tracks_passed = 0
+        x_tracks_available = len(x_tracks)
+        while x_tracks_passed < x_tracks_available:
+            # in case if we are not in inverted mode - we going from first to last track
+            # in case if we are in inverted - we going from last to first track
+            if not inverted:
+                track = x_tracks[x_tracks_passed]
             else:
-                track = tracks[tracks_passed]
-                gcode_file.write("G1 X%.2f00 Y%.2f00 F1500\n" % ((track[0] * mm_per_dot), (line * mm_per_dot)))
-                gcode_file.write("M106 S1 \n")
-                gcode_file.write("G1 X%.2f00 Y%.2f00 F900 \n" % ((track[1] * mm_per_dot), (line * mm_per_dot)))
-                gcode_file.write("M107 \n")
-            tracks_passed+=1
+                track = x_tracks[x_tracks_available - x_tracks_passed - 1]
+
+            # in case if we are not in inverted mode - we going from point 0 to point 1
+            # in case if we are in inverted mode - we going from point 1 to point 1
+            gcode_file.write("G1 X%.2f00 Y%.2f00 F%i\n" % ((track[inverted] * mm_per_dot), (y * mm_per_dot), laser_fast))
+            gcode_file.write("%s \n" % laser_on)
+            gcode_file.write("G1 X%.2f00 Y%.2f00 F%i\n" % ((track[not inverted] * mm_per_dot), (y * mm_per_dot), laser_slow))
+            gcode_file.write("%s \n" % laser_off)
+            x_tracks_passed+=1
         inverted = not inverted
+    
+    print("GCode generation is done")
     # -------------- End of linear laser track searching ----------
 
 # writing GCode footer and closing a file
 gcode_file.write(gcode_footer)
 gcode_file.close()
 
-print("\n\nGcode file generated: %s" % args.gcode_filename)
+print("\n\nGcode stored to file: %s" % args.gcode_filename)
